28eb30326141bcd551b5ec42160cdf5a0b06a960
Jeremieon <jerry_slade@outlook.com>
2022-11-08 16:33:59 +0100

    added base template

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('about/', views.about, name='about'),
+    path('<str:name>/', views.greet, name='greet'),
+    path('user/<int:id>/', views.users, name='number'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+tasks = ['foo','bar','baz']
+posts = [
+    {
+        'author': 'Anna',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date': 'August 10, 2020'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog post 2',
+        'content': 'First post content',
+        'date': 'August 1, 2020'
+    }
+]
 # Create your views here.
+def index(request):
+    return render(request,'blog/index.html',{'tasks':tasks,
+                                              'posts':posts})
+def about(request):
+    return HttpResponse('<p>This is about page</p>')
+def greet(request,name):
+    return HttpResponse(f'<p>Hello { name.capitalize() }!</p>')
+def users(request,id):
+    return HttpResponse(f'<p>Hello User, { id }</p>')
