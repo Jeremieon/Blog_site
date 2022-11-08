77648f29e22e4f410d08743d520b5883f51436a2
Jeremieon <jerry_slade@outlook.com>
2022-11-08 16:57:18 +0100

    added database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-tasks = ['foo','bar','baz']
-posts = [
-    {
-        'author': 'Anna',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date': 'August 10, 2020'
-    },
-    {
-        'author': 'Jane',
-        'title': 'Blog post 2',
-        'content': 'First post content',
-        'date': 'August 1, 2020'
-    }
-]
+from .models import Post
 # Create your views here.
 def index(request):
-    return render(request,'blog/index.html',{'tasks':tasks,
-                                              'posts':posts})
+
+    return render(request,'blog/index.html',{'posts':Post.objects.all()})
 def about(request):
     return HttpResponse('<p>This is about page</p>')
 def greet(request,name):
